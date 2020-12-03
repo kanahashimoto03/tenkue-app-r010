bb5748518107f4c425b458897b5bd74a4b545c32
hidesan4869 <t1123.gogo@gmail.com>
2020-12-03 23:52:01 +0900

    「open」classから「is-active」に変更

--- src/asset/js/app.js
+++ src/asset/js/app.js
@@ -1,5 +1,5 @@
 const isOpen = document.querySelector('#navbtn');
 
 isOpen.addEventListener('click', () => {
-  document.querySelector('body').classList.toggle('open');
+  document.querySelector('body').classList.toggle('is-active');
 });
